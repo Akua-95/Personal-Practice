9135bc7a38f6c805b3e3a97f9229c97c84c03d76
Akua-95 <serwaa.precious@gmail.com>
2023-05-20 05:31:17 -0700

    task.c

---
+++ task.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print values between 0 and 50
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 49)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
